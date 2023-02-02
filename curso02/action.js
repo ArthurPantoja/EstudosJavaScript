a00f199955f598b128071994411fece90e7f142f
ArthurPantoja <arthurcristian.180@gmail.com>
2023-02-02 11:49:07 -0300

    commit do dia 02/01

--- curso02/action.js
+++ curso02/action.js
@@ -1,3 +1,12 @@
 function alerta() {
     alert("Esta pagina foi desenvolvida por Arthur Cristia com intuito de ajudar nos seus proprios conhecimentosde JavaScript")
 }
+
+var vel = 200
+
+console.log(`A velocidade de seu carro e de ${vel}`)
+
+if(vel > 80){
+    console.log("Você ultrapassou o limite de velocidade. Multado! ")
+}
+console.log("Não esqueça de usar sinto de segurança!")
